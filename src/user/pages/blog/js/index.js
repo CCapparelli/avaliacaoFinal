e5143a91a116a6f8b38322026a7ea8a1a5644c33
CCapparelli <kdcapparelli@gmail.com>
2024-10-10 12:24:11 -0300

    Update
    Dia 3, manha 2

--- src/user/pages/blog/js/index.js
+++ src/user/pages/blog/js/index.js
@@ -1,20 +1,31 @@
 var posts = $('#posts');
-var postItems = $('.post');
+var postItems = document.querySelectorAll('.post');
 
 function applyTheme() {
     
     if (currentTheme() === 'dark') {
         posts.css('backgroundColor', 'rgb(80,80,80)'); 
-        postItems.each((_,val) => {
-            val.sytle.backgroundColor = 'rgb(60,60,60)';
-            // val.css('backgroundColor', 'rgb(60,60,60)'); 
-            // val.attr('style', 'background-color: rgb(60,60,60);');
+        postItems.forEach(element => {
+            element.sytle.backgroundColor = 'lime';
         });
+        // postItems.each((_,val) => {
+        //     val.sytle.backgroundColor = 'rgb(60,60,60)';
+        //     // val.css('backgroundColor', 'rgb(60,60,60)'); 
+        //     // val.attr('style', 'background-color: rgb(60,60,60);');
+        // });
     } else {
         posts.css('backgroundColor', 'silver');
-        postItems.each((_,val) => {
-            val.sytle.backgroundColor = 'white';
-            // val.css('backgroundColor', 'white'); 
-            // val.attr('style', 'background-color: white;');
-        });}
+        postItems.forEach(element => {
+            element.sytle.backgroundColor = 'pink';
+        });
+        // postItems.each((_,val) => {
+        //     val.sytle.backgroundColor = 'white';
+        //     // val.css('backgroundColor', 'white'); 
+        //     // val.attr('style', 'background-color: white;');
+        // });}
+    }
+
+// for (let el of postItems) 
+//     el.style.backgroundColor = 'yellow';
+
 }
